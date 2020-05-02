a71893d51bd35b983c31ccc63529c93c3874a266
FelipeM4398 <luisf4398@hotmail.com>
2020-05-02 16:51:49 -0500

    Consumir servicios para registrarse e iniciar sesión

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { InicioComponent } from './componentes/inicio/inicio.component';
-import { LoginComponent } from './componentes/login/login.component';
-import { RegistrarComponent } from './componentes/registrar/registrar.component';
-import { PerfilComponent } from './componentes/perfil/perfil.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule} from '@angular/common/http';
-
+import { AppComponent } from "./app.component";
+import { InicioComponent } from "./views/inicio/inicio.component";
+import { LoginComponent } from "./views/login/login.component";
+import { RegistrarComponent } from "./views/registrar/registrar.component";
+import { PerfilComponent } from "./views/perfil/perfil.component";
+import { InputTextComponent } from "./components/input-text/input-text.component";
+import { FormLoginComponent } from "./components/form-login/form-login.component";
+import { FormRegisterComponent } from './components/form-register/form-register.component';
+import { MessageComponent } from './components/message/message.component';
 
 @NgModule({
   declarations: [
@@ -16,15 +20,19 @@ import { HttpClientModule} from '@angular/common/http';
     InicioComponent,
     LoginComponent,
     RegistrarComponent,
-    PerfilComponent
+    PerfilComponent,
+    InputTextComponent,
+    FormLoginComponent,
+    FormRegisterComponent,
+    MessageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
-    
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/utils/constants.ts
@@ -0,0 +1,6 @@
+const API_URL = "https://seminario-tercer-corte.herokuapp.com/api/v1/";
+export const constants = {
+  loginUrl: `${API_URL}auth/token`,
+  registerUrl: `${API_URL}auth/signup`,
+  profileUrl: `${API_URL}auth/profile`,
+};
